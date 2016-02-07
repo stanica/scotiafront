2d53bb9f31de03250962e9845eaa1819084a2db3
Alex Craig <acraig780@gmail.com>
2016-02-07 00:31:14 -0500

    profile page

--- assets/scripts/controllers/profile-ctrl.js
+++ assets/scripts/controllers/profile-ctrl.js
@@ -1,7 +1,15 @@
 Scotiafront.controller('ProfileCtrl', ['$scope', '$routeParams', 'API', function($scope, $routeParams, API) {
-	API.getUserByID($routeParams.userid).then(function(data) {
-		$scope.user = data;
-	});
+	// API.getUserByID($routeParams.userid).then(function(data) {
+	// 	$scope.user = data;
+	// });
 
-	
+	$scope.user = {
+		name: 'Tyler Smith',
+		employed: false,
+		location: 'Toronto, Ontario',
+		currentamount: 7500,
+		totalamount: 10000,
+	};
+
+	$scope.amountpercent = (($scope.user.currentamount / $scope.user.totalamount) * 100).toFixed(0);
 }]);
